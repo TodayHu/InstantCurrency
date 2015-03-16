1e64b42bbfc2c8c466200d3498cc3f96aebe03ed
Hisma Mulya S <mamaz@icehousecorp.com>
2015-03-16 14:57:42 +0700

    Initial Commit

---
+++ InstantCurrency/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstantCurrency
+//
+//  Created by Hisma Mulya S on 3/16/15.
+//  Copyright (c) 2015 Ice House. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
